d63dead75d88a84d6dc898f6be0cb831d4eec6b2
hassan ali <hassanali0111272@gmail.com>
2024-12-13 22:33:00 +0500

    yet to fix

--- courses/CSS/cssQuiz.js
+++ courses/CSS/cssQuiz.js
@@ -1,132 +1,244 @@
-var htmlQuiz = [
-    {
-      question: "HTML Stands for ?",
-      option1: "Hypertext",
-      option2: "Markup",
-      option3: "HyperSuperText",
-      option4: "HyperText Markup Language",
-      answer: "HyperText Markup Language",
-    },
-    {
-      question: "Which Element used to bold text in HTML?",
-      option1: "img",
-      option2: "h1",
-      option3: "strong",
-      option4: "p",
-      answer: "strong",
-    },
-    {
-      question: "Which Element used for Image in HTML?",
-      option1: "span",
-      option2: "div",
-      option3: "image",
-      option4: "img",
-      answer: "img",
-    },
-  ];
-  
-  let quizContainer = document.querySelector(".quiz-container");
-  
-  let question = document.querySelector(".question");
-  
-  let option1 = document.querySelector("#option1");
-  
-  let option2 = document.querySelector("#option2");
-  
-  let option3 = document.querySelector("#option3");
-  
-  let option4 = document.querySelector("#option4");
-  
-  let label1 = document.querySelector("#label1");
-  
-  let label2 = document.querySelector("#label2");
-  
-  let label3 = document.querySelector("#label3");
-  
-  let label4 = document.querySelector("#label4");
-  
-  let option = document.getElementsByName("option");
-  console.log(option);
-  
-  let nextBtn = document.querySelector(".nextBtn");
-  
-  let questionNumber = 0;
-  
-  let score = 0;
-  
-  let resultContainer = document.querySelector(".result-container");
-  
-  let resultMessage = document.querySelector(".resulMessage")
-  
-  let percentageShow = document.querySelector(".percentage");
-  
-  let totalQue = document.querySelector(".totalQues")
-  
-  let correcQues = document.querySelector(".correctQues")
-  
-  function loadQuestion() {
-    question.innerHTML = htmlQuiz[questionNumber].question;
-    label1.innerHTML = htmlQuiz[questionNumber].option1;
-    label2.innerHTML = htmlQuiz[questionNumber].option2;
-    label3.innerHTML = htmlQuiz[questionNumber].option3;
-    label4.innerHTML = htmlQuiz[questionNumber].option4;
-  
-    option1.value = htmlQuiz[questionNumber].option1;
-    option2.value = htmlQuiz[questionNumber].option2;
-    option3.value = htmlQuiz[questionNumber].option3;
-    option4.value = htmlQuiz[questionNumber].option4;
-  
-  }
-  nextBtn.addEventListener("click", function () {
-    let inputChecked = false;
-    for (let i = 0; i < option.length; i++) {
-      if (option[i].checked) {
-        inputChecked = true;
-  
-        if (option[i].value === htmlQuiz[questionNumber].answer) {
-          score++;
-          console.log(score);
-        }
+var cssQuiz = [
+  {
+    question: "What does CSS stand for?",
+    option1: "Cascading Style Sheets",
+    option2: "Creative Style Sheets",
+    option3: "Computer Style Sheets",
+    option4: "Colorful Style Sheets",
+    answer: "Cascading Style Sheets",
+  },
+  {
+    question: "Which HTML tag is used to define an internal style sheet?",
+    option1: "css",
+    option2: "style",
+    option3: "script",
+    option4: "link",
+    answer: "style",
+  },
+  {
+    question:
+      "Which property is used to change the background color of an element?",
+    option1: "background-color",
+    option2: "color",
+    option3: "bgcolor",
+    option4: "background",
+    answer: "background-color",
+  },
+  {
+    question: "How do you add an external CSS file to an HTML document?",
+    option1: "stylesheet",
+    option2: "style src='style.css",
+    option3: "link rel='stylesheet' href='style.css",
+    option4: "css href='style.css'>",
+    answer: "link rel='stylesheet' href='style.css'",
+  },
+  {
+    question: "Which property is used to change the font size of text?",
+    option1: "font-style",
+    option2: "text-size",
+    option3: "font-size",
+    option4: "size",
+    answer: "font-size",
+  },
+  {
+    question: "How do you make text bold in CSS?",
+    option1: "font-weight: bold;",
+    option2: "font-style: bold;",
+    option3: "text-weight: bold;",
+    option4: "text-style: bold;",
+    answer: "font-weight: bold;",
+  },
+  {
+    question: "Which property is used to change the text color of an element?",
+    option1: "font-color",
+    option2: "text-color",
+    option3: "color",
+    option4: "foreground-color",
+    answer: "color",
+  },
+  {
+    question:
+      "Which CSS property controls the layout of items in a flex container?",
+    option1: "display",
+    option2: "justify-content",
+    option3: "align-items",
+    option4: "flex-direction",
+    answer: "display",
+  },
+  {
+    question: "What is the default position value of an HTML element?",
+    option1: "absolute",
+    option2: "relative",
+    option3: "fixed",
+    option4: "static",
+    answer: "static",
+  },
+  {
+    question: "How do you center a block element horizontally using CSS?",
+    option1: "text-align: center;",
+    option2: "margin: 0 auto;",
+    option3: "align-items: center;",
+    option4: "display: center;",
+    answer: "margin: 0 auto;",
+  },
+  {
+    question:
+      "Which property is used to set the space between the border and the content of an element?",
+    option1: "margin",
+    option2: "padding",
+    option3: "border-spacing",
+    option4: "spacing",
+    answer: "padding",
+  },
+  {
+    question: "What does the z-index property in CSS control?",
+    option1: "The stacking order of elements",
+    option2: "The transparency of an element",
+    option3: "The size of an element",
+    option4: "The positioning of an element",
+    answer: "The stacking order of elements",
+  },
+  {
+    question:
+      "Which property is used to create rounded corners for an element?",
+    option1: "border-radius",
+    option2: "corner-radius",
+    option3: "border-round",
+    option4: "corner-round",
+    answer: "border-radius",
+  },
+  {
+    question: "How do you apply a shadow to an element in CSS?",
+    option1: "box-shadow",
+    option2: "shadow-box",
+    option3: "text-shadow",
+    option4: "element-shadow",
+    answer: "box-shadow",
+  },
+  {
+    question:
+      "Which property is used to set the spacing between lines of text?",
+    option1: "line-height",
+    option2: "letter-spacing",
+    option3: "text-indent",
+    option4: "text-spacing",
+    answer: "line-height",
+  },
+];
+
+let quizContainer = document.querySelector(".quiz-container");
+
+let question = document.querySelector(".question");
+
+let option1 = document.querySelector("#option1");
+
+let option2 = document.querySelector("#option2");
+
+let option3 = document.querySelector("#option3");
+
+let option4 = document.querySelector("#option4");
+
+let label1 = document.querySelector("#label1");
+
+let label2 = document.querySelector("#label2");
+
+let label3 = document.querySelector("#label3");
+
+let label4 = document.querySelector("#label4");
+
+let option = document.getElementsByName("option");
+
+let nextBtn = document.querySelector(".nextBtn");
+
+let questionNumber = 0;
+
+let score = 0;
+
+let resultContainer = document.querySelector(".result-container");
+
+let resultMessage = document.querySelector(".resulMessage");
+
+let percentageShow = document.querySelector(".percentage");
+
+let totalQue = document.querySelector(".totalQues");
+
+let correcQues = document.querySelector(".correctQues");
+
+function loadQuestion() {
+  question.innerHTML = cssQuiz[questionNumber].question;
+  label1.innerHTML = cssQuiz[questionNumber].option1;
+  label2.innerHTML = cssQuiz[questionNumber].option2;
+  label3.innerHTML = cssQuiz[questionNumber].option3;
+  label4.innerHTML = cssQuiz[questionNumber].option4;
+
+  option1.value = cssQuiz[questionNumber].option1;
+  option2.value = cssQuiz[questionNumber].option2;
+  option3.value = cssQuiz[questionNumber].option3;
+  option4.value = cssQuiz[questionNumber].option4;
+}
+nextBtn.addEventListener("click", function () {
+  let inputChecked = false;
+  for (let i = 0; i < option.length; i++) {
+    if (option[i].checked) {
+      inputChecked = true;
+
+      if (option[i].value === cssQuiz[questionNumber].answer) {
+        score++;
       }
     }
-    if (!inputChecked) {
-      Swal.fire({
-          title: "No Option Selected",
-          text: "Please Select Any Option",
-          icon: "error",
-        });
+  }
+  if (!inputChecked) {
+    Swal.fire({
+      title: "No Option Selected",
+      text: "Please Select Any Option",
+      icon: "error",
+    });
+  } else {
+    if (questionNumber < cssQuiz.length - 1) {
+      questionNumber++;
+      deSelectInput();
+      loadQuestion();
     } else {
-      if (questionNumber < htmlQuiz.length - 1) {
-        questionNumber++;
-        deSelectInput();
-        loadQuestion();
-      } else{
-          quizContainer.style.display = "none"
-        resultContainer.style.display = "flex";
-        let percentage = Math.round((score / htmlQuiz.length) * 100);
-        if(percentage > 70){
-          resultMessage.innerHTML = "Congradualations";
-          resultMessage.style.color = "green";
-          percentageShow.innerHTML = `${percentage}%`;
-          totalQue.innerHTML = htmlQuiz.length;
-          correcQues.innerHTML = score;
+      let results = [];
+      let result = {};
+      quizContainer.style.display = "none";
+      resultContainer.style.display = "flex";
+      let percentage = Math.round((score / cssQuiz.length) * 100);
+      if (percentage > 70) {
+        resultMessage.innerHTML = "Congradualations";
+        resultMessage.style.color = "green";
+        percentageShow.innerHTML = `${percentage}%`;
+        totalQue.innerHTML = cssQuiz.length;
+        correcQues.innerHTML = score;
       } else {
-          resultMessage.innerHTML = "OOps you're fail better next time";
-          resultMessage.style.color = "red";
-          percentageShow.innerHTML = `${percentage}%`;
-          totalQue.innerHTML = htmlQuiz.length;
-          correcQues.innerHTML = score;
+        resultMessage.innerHTML = "OOps you're fail better next time";
+        resultMessage.style.color = "red";
+        percentageShow.innerHTML = `${percentage}%`;
+        totalQue.innerHTML = cssQuiz.length;
+        correcQues.innerHTML = score;
       }
-      }
-  
-    }
-  });
-  
-  function deSelectInput() {
-    for (let i = 0; i < option.length; i++) {
-      option[i].checked = false;
+      result.totalQue = cssQuiz.length;
+      result.score = score;
+      result.scoreInPercent = percentage;
+      results.push(result);
+
+      let resultData = JSON.stringify(results);
+      localStorage.setItem("resultData", resultData);
+
+      resultD();
     }
   }
-  
-  window.onload = loadQuestion();
-  
+});
+
+function deSelectInput() {
+  for (let i = 0; i < option.length; i++) {
+    option[i].checked = false;
+  }
+}
+
+window.onload = loadQuestion();
+function resultD() {
+  let data = localStorage.getItem("resultData");
+  let newData = JSON.parse(data);
+  console.log(newData);
+}
--- courses/JS/jsQuiz.js
+++ courses/JS/jsQuiz.js
@@ -1,29 +1,125 @@
-var htmlQuiz = [
-    {
-      question: "HTML Stands for ?",
-      option1: "Hypertext",
-      option2: "Markup",
-      option3: "HyperSuperText",
-      option4: "HyperText Markup Language",
-      answer: "HyperText Markup Language",
-    },
-    {
-      question: "Which Element used to bold text in HTML?",
-      option1: "img",
-      option2: "h1",
-      option3: "strong",
-      option4: "p",
-      answer: "strong",
-    },
-    {
-      question: "Which Element used for Image in HTML?",
-      option1: "span",
-      option2: "div",
-      option3: "image",
-      option4: "img",
-      answer: "img",
-    },
-  ];
+var jsQuiz = [
+  {
+    question: "Which of the following is a JavaScript data type?",
+    option1: "String",
+    option2: "Number",
+    option3: "Boolean",
+    option4: "All of the above",
+    answer: "All of the above",
+  },
+  {
+    question: "Which keyword is used to declare a variable in JavaScript?",
+    option1: "var",
+    option2: "let",
+    option3: "const",
+    option4: "All of the above",
+    answer: "All of the above",
+  },
+  {
+    question: "How do you write 'Hello World' in an alert box in JavaScript?",
+    option1: "alert('Hello World');",
+    option2: "msg('Hello World');",
+    option3: "console.log('Hello World');",
+    option4: "print('Hello World');",
+    answer: "alert('Hello World');",
+  },
+  {
+    question: "Which operator is used for strict equality in JavaScript?",
+    option1: "=",
+    option2: "==",
+    option3: "===",
+    option4: "!==",
+    answer: "===",
+  },
+  {
+    question: "What is the output of: `typeof null`?",
+    option1: "'null'",
+    option2: "'undefined'",
+    option3: "'object'",
+    option4: "'number'",
+    answer: "'object'",
+  },
+  {
+    question: "Which method is used to parse a JSON string into a JavaScript object?",
+    option1: "JSON.stringify()",
+    option2: "JSON.parse()",
+    option3: "JSON.objectify()",
+    option4: "JSON.toObject()",
+    answer: "JSON.parse()",
+  },
+  {
+    question: "What is the correct syntax for a JavaScript arrow function?",
+    option1: "() => { return 'Hello'; }",
+    option2: "function() => { return 'Hello'; }",
+    option3: "() -> { return 'Hello'; }",
+    option4: "function => { return 'Hello'; }",
+    answer: "() => { return 'Hello'; }",
+  },
+  {
+    question: "Which method adds a new element to the end of an array?",
+    option1: "push()",
+    option2: "pop()",
+    option3: "shift()",
+    option4: "unshift()",
+    answer: "push()",
+  },
+  {
+    question: "Which keyword is used to define a constant in JavaScript?",
+    option1: "var",
+    option2: "let",
+    option3: "const",
+    option4: "constant",
+    answer: "const",
+  },
+  {
+    question: "What is the correct way to create an object in JavaScript?",
+    option1: "let obj = {key: 'value'};",
+    option2: "let obj = (key: 'value');",
+    option3: "let obj = [key: 'value'];",
+    option4: "let obj = 'key: value';",
+    answer: "let obj = {key: 'value'};",
+  },
+  {
+    question: "What is the purpose of the `isNaN()` function?",
+    option1: "To check if a value is null",
+    option2: "To check if a value is not a number",
+    option3: "To convert a string to a number",
+    option4: "To check if a value is undefined",
+    answer: "To check if a value is not a number",
+  },
+  {
+    question: "Which JavaScript loop is guaranteed to run at least once?",
+    option1: "for loop",
+    option2: "while loop",
+    option3: "do-while loop",
+    option4: "for-of loop",
+    answer: "do-while loop",
+  },
+  {
+    question: "How can you add a comment in JavaScript?",
+    option1: "// This is a comment",
+    option2: "<!-- This is a comment -->",
+    option3: "' This is a comment",
+    option4: "# This is a comment",
+    answer: "// This is a comment",
+  },
+  {
+    question: "What does the `map()` method do in JavaScript?",
+    option1: "Iterates over an array and filters elements",
+    option2: "Applies a function to each element and creates a new array",
+    option3: "Finds the first matching element in an array",
+    option4: "Reduces an array to a single value",
+    answer: "Applies a function to each element and creates a new array",
+  },
+  {
+    question: "What is the purpose of the `this` keyword in JavaScript?",
+    option1: "To refer to the current object",
+    option2: "To define a variable",
+    option3: "To declare a function",
+    option4: "To create a class",
+    answer: "To refer to the current object",
+  },
+];
   
   let quizContainer = document.querySelector(".quiz-container");
   
@@ -46,7 +142,7 @@ var htmlQuiz = [
   let label4 = document.querySelector("#label4");
   
   let option = document.getElementsByName("option");
-  console.log(option);
+
   
   let nextBtn = document.querySelector(".nextBtn");
   
@@ -65,16 +161,16 @@ var htmlQuiz = [
   let correcQues = document.querySelector(".correctQues")
   
   function loadQuestion() {
-    question.innerHTML = htmlQuiz[questionNumber].question;
-    label1.innerHTML = htmlQuiz[questionNumber].option1;
-    label2.innerHTML = htmlQuiz[questionNumber].option2;
-    label3.innerHTML = htmlQuiz[questionNumber].option3;
-    label4.innerHTML = htmlQuiz[questionNumber].option4;
+    question.innerHTML = jsQuiz[questionNumber].question;
+    label1.innerHTML = jsQuiz[questionNumber].option1;
+    label2.innerHTML = jsQuiz[questionNumber].option2;
+    label3.innerHTML = jsQuiz[questionNumber].option3;
+    label4.innerHTML = jsQuiz[questionNumber].option4;
   
-    option1.value = htmlQuiz[questionNumber].option1;
-    option2.value = htmlQuiz[questionNumber].option2;
-    option3.value = htmlQuiz[questionNumber].option3;
-    option4.value = htmlQuiz[questionNumber].option4;
+    option1.value = jsQuiz[questionNumber].option1;
+    option2.value = jsQuiz[questionNumber].option2;
+    option3.value = jsQuiz[questionNumber].option3;
+    option4.value = jsQuiz[questionNumber].option4;
   
   }
   nextBtn.addEventListener("click", function () {
@@ -83,9 +179,9 @@ var htmlQuiz = [
       if (option[i].checked) {
         inputChecked = true;
   
-        if (option[i].value === htmlQuiz[questionNumber].answer) {
+        if (option[i].value === jsQuiz[questionNumber].answer) {
           score++;
-          console.log(score);
+          
         }
       }
     }
@@ -96,27 +192,38 @@ var htmlQuiz = [
           icon: "error",
         });
     } else {
-      if (questionNumber < htmlQuiz.length - 1) {
+      if (questionNumber < jsQuiz.length - 1) {
         questionNumber++;
         deSelectInput();
         loadQuestion();
       } else{
+        let results = [];
+      let result = {};
           quizContainer.style.display = "none"
         resultContainer.style.display = "flex";
-        let percentage = Math.round((score / htmlQuiz.length) * 100);
+        let percentage = Math.round((score / jsQuiz.length) * 100);
         if(percentage > 70){
           resultMessage.innerHTML = "Congradualations";
           resultMessage.style.color = "green";
           percentageShow.innerHTML = `${percentage}%`;
-          totalQue.innerHTML = htmlQuiz.length;
+          totalQue.innerHTML = jsQuiz.length;
           correcQues.innerHTML = score;
       } else {
           resultMessage.innerHTML = "OOps you're fail better next time";
           resultMessage.style.color = "red";
           percentageShow.innerHTML = `${percentage}%`;
-          totalQue.innerHTML = htmlQuiz.length;
+          totalQue.innerHTML = jsQuiz.length;
           correcQues.innerHTML = score;
       }
+      result.totalQue = jsQuiz.length;
+      result.score = score;
+      result.scoreInPercent = percentage;
+      results.push(result);
+
+      let resultData = JSON.stringify(results);
+      localStorage.setItem("resultData", resultData);
+
+      resultD();
       }
   
     }
@@ -129,4 +236,8 @@ var htmlQuiz = [
   }
   
   window.onload = loadQuestion();
-  
+  function resultD() {
+    let data = localStorage.getItem("resultData");
+    let newData = JSON.parse(data);
+    console.log(newData);
+  }
