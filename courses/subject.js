b10b03f2d59ce16cafc06175a47ce727949a88a5
hassan ali <hassanali0111272@gmail.com>
2024-12-19 20:54:01 +0500

    logout error fixed

--- courses/subject.js
+++ courses/subject.js
@@ -5,4 +5,17 @@
   }
 }
 
+//  function checkIsLoggedIn() {
+//    let isLoggedIn = localStorage.getItem("isLoggedIn");
+//    if (isLoggedIn !== "true") {
+//      window.location.href = "../../index.html";
+//    }
+//  }
+ window.onload = checkIsLoggedIn();
+ let logout = document.querySelector(".logout");
+ console.log(logout)
+ logout.addEventListener("click", () => {
+   localStorage.setItem("isLoggedIn", false);
+   checkIsLoggedIn();
+ });
 window.onload = checkIsLoggedIn()
